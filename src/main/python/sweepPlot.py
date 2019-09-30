d3eac55cc301adafc67f13fc9792cc47490186b7
Aaron Schutza <aaronschutza@gmail.com>
2019-09-30 17:36:08 -0500

    started work on plotting tines #16

--- src/main/python/sweepPlot.py
+++ src/main/python/sweepPlot.py
@@ -4,6 +4,7 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import os
 import glob
+from anytree import Node, RenderTree, AsciiStyle, find_by_attr
 
 list_of_files = glob.glob('/home/aaron/topl/ssh/delaySweep1/*.tree') # * means all if need specific format then *.csv
 
@@ -12,30 +13,37 @@ yaxis = np.empty([len(list_of_files)])
 zaxis = np.empty([len(list_of_files)])
 taxis = np.empty([len(list_of_files)])
 baxis = np.empty([len(list_of_files)])
+tines = np.empty([len(list_of_files)])
 print(len(list_of_files))
 i = 0
 for file in list_of_files:
 	f = open(file)
 	tree = json.load(f)
 	f.close
+	ii = 0
 	j = 0
+	jj = 0
+	jjj = 0
+	k = 1
 	for s in tree["data"]:
 		if s["history"][0]["id"] != "":
 			j = j + 1
-	zaxis[i] = j
-	j = 0
-	k = 1
-	for s in tree["data"]:
 		l = len(s["history"])
 		if l < k:
-			j = j + 1
+			jj = jj + 1
 		k = l
-	taxis[i] = j
-	j = 0
-	for s in tree["data"]:
-		l = len(s["blocks"])
-		j = j + l
-	baxis[i] = j
+		ll = len(s["blocks"])
+		jjj = jjj + ll
+		if i == 100:
+			for b in s["blocks"]:
+				if ii == 0:
+					root = Node(b["id"])
+				else:
+					Node(b["id"],parent = find_by_attr(root,b["pid"]))
+		ii = ii + 1
+	zaxis[i] = j
+	taxis[i] = jj
+	baxis[i] = jjj
 	xaxis[i] = float(tree["info"]["delay_ms_km"])
 	yaxis[i] = float(tree["info"]["f_s"])
 	i = i + 1
@@ -46,7 +54,7 @@ ax.scatter3D(xaxis,yaxis,zaxis, c=zaxis, cmap='hsv')
 ax.set_xlabel('Delay (ms/km)')
 ax.set_ylabel('Active Slot Coefficient')
 ax.set_zlabel('Block Number')
-ax.set_title('Total blocks on chain after 200 slots')
+ax.set_title('Total blocks on chain after 1000 slots')
 plt.show()
 
 plt.figure(2)
@@ -55,7 +63,7 @@ ax.scatter3D(xaxis,yaxis,taxis, c=taxis, cmap='hsv')
 ax.set_xlabel('Delay (ms/km)')
 ax.set_ylabel('Active Slot Coefficient')
 ax.set_zlabel('Number of Tines')
-ax.set_title('Total tines after 200 slots')
+ax.set_title('Total reorgs after 1000 slots')
 plt.show()
 
 plt.figure(3)
@@ -64,5 +72,5 @@ ax.scatter3D(xaxis,yaxis,baxis, c=baxis, cmap='hsv')
 ax.set_xlabel('Delay (ms/km)')
 ax.set_ylabel('Active Slot Coefficient')
 ax.set_zlabel('Number of Blocks')
-ax.set_title('Total blocks after 200 slots')
+ax.set_title('Total blocks after 1000 slots')
 plt.show()
